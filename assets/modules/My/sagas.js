220b34845299e40d1e73b3013950c42e3ca47b1c
JOHNFLEURIMOND <johncfleurimond@gmail.com>
2019-11-02 12:38:51 -0400

    first commit

---
+++ assets/modules/My/sagas.js
@@ -0,0 +1,24 @@
+import { put, takeEvery, call, all, select } from 'redux-saga/effects';
+import axios from 'axios';
+
+function fetchQuakes() {
+    return axios.get('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson');
+}
+
+export function* handleLoadQuakes() {
+    try {
+        console.log('handleLoadQuakes');
+        let resp = yield call(fetchQuakes);
+
+        yield put({
+            type: 'SET_QUAKES',
+            quakes: resp.data.features,
+        });
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+export function* watchRefreshQuakes() {
+    yield takeEvery('REFRESH_QUAKES', handleLoadQuakes);
+}
